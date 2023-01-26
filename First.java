6c27a995ac6367a8fa98f224b026385baaf14ab5
assaf.lello <assaflello30@gmail.com>
2023-01-26 11:39:41 +0200

    First one is done

---
+++ First.java
@@ -0,0 +1,10 @@
+public class First {
+	public static void main (String []args ) {
+		System.out.print("This is my ");
+		System.out.println("First java program");
+		System.out.println("Programming is fun!");
+}
+
+
+
+}
